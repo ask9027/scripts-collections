c8b7ed846f3453475c0b49710f6ced62ac012a16
Arvind Kaushik <arvindkaushik.kaushik90@gmail.com>
2023-07-02 09:43:56 +0530

    refactor code

--- xiaomi-bloatware/uninstall.sh
+++ xiaomi-bloatware/uninstall.sh
@@ -2,36 +2,39 @@ echo "Uninstalling Unwanted Packages"
 
 filename="packages.txt"
 start=1
-pacnumbers=$(wc -l < $filename)
+pacnumbers=$(wc -l <$filename)
 
-removepac(){
-    while [ $start -le $pacnumbers ]; do
-        pac=`sed -e "${start}q;d" $filename`
-        printf "No.$start: $pac Uninstalling ..."; echo
-        sudo pm uninstall --user 0 -k $pac
-        ((++start))
-    done
+function removepac() {
+	while [ $start -le $pacnumbers ]; do
+		pac=$(sed -e "${start}q;d" $filename)
+		printf "No.$start: $pac Uninstalling ..."
+		echo
+		sudo pm uninstall --user 0 -k $pac
+		((++start))
+	done
 }
 
-checkroot(){
-    printf "Does Your Devices rooted? (y/N): "
-    read r
-    if [ $r = "y" ] || [ $r = "Y" ]; then
-	if [ -f "$PREFIX/bin/sudo" ]; then
-	    removepac
+function checkroot() {
+	printf "Does Your Devices rooted? (y/N): "
+	read r
+	if [ $r = "y" ] || [ $r = "Y" ]; then
+		if [ -f "$PREFIX/bin/sudo" ]; then
+			removepac
+		else
+			printf "Install tsu first.\nDo you want to install it? (y/N): "
+			read p
+			if [ $p = "y" ] || [ $p = "Y" ]; then
+				pkg install tsu
+				removepac
+			else
+				printf "use ADB"
+				echo
+			fi
+		fi
 	else
-	    printf "Install tsu first.\nDo you want to install it? (y/N): "
-	    read p
-	    if [ $p = "y" ] || [ $p = "Y" ]; then
-		pkg install tsu
-		removepac
-	    else
-		printf "use ADB"; echo
-	    fi
+		printf "You should root devices or use ADB"
+		echo
 	fi
-    else
-	printf "You should root devices or use ADB"; echo
-    fi
 }
 
 checkroot
